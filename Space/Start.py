a1350a2d21fafc1548743aa598ef74fa5e4a2336
Aecio1001 <aeciopaulo29@gmail.com>
2021-02-15 18:12:53 -0300

    Cor de plano de fundo

--- Space/Start.py
+++ Space/Start.py
@@ -7,6 +7,9 @@ def run_game():
 	screen = pygame.display.set_mode((1200, 800))
 	pygame.display.set_caption("Alien Invasion")
 
+	# Define a cor do plano de fundo
+	bg_color = (230, 230, 230)
+
 	#Inicia o laço principal do jogo
 	while True:
 
@@ -14,6 +17,9 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+
+		#Redesenha a tela a cada passagem pelo laço
+		screen.fill(bg_color)
 
 		#Deixa a tela mais recente visivel
 		pygame.display.flip()
