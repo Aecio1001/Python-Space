04786be58658cf055800d587f124480d7283ebf4
Aecio1001 <aeciopaulo29@gmail.com>
2021-02-17 22:32:36 -0300

    adição de espaçonave

--- Space/Alien_invasion.py
+++ Space/Alien_invasion.py
@@ -1,5 +1,7 @@
 # Importes de bibliotecas 
 import sys, pygame
+from setting import Setting 
+from ship import Ship
 
 def run_game():
 	#Inicializa o jogo e cria um objeto para a tela
@@ -7,6 +9,9 @@ def run_game():
 	ia_settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+
+	# Cria uma espaçonave
+	ship = Ship(screen)
 
 	#Inicia o laço principal do jogo
 	while True:
@@ -18,6 +23,7 @@ def run_game():
 
 		#Redesenha a tela a cada passagem pelo laço
 		screen.fill(ai_settings.bg_color)
+		ship.blitme()
 
 		#Deixa a tela mais recente visivel
 		pygame.display.flip()
