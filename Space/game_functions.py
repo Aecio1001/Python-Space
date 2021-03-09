3bb40c8985844188f3e6d344d09299c666074a30
Aecio1001 <aeciopaulo29@gmail.com>
2021-03-09 17:34:52 -0300

    Limitações laterais de navegação de objeto

--- Space/game_functions.py
+++ Space/game_functions.py
@@ -1,6 +1,21 @@
 # Importando bibliotecas
 import sys, pygame
 
+def check_keydown_events(event, ship):
+	'''Responde a pressionamentos de tecla.'''
+	if event.key == pygame.K_RIGHT:
+		#Move a espaçonave para a direita
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+
+def check_keyup_events(event, ship):
+	'''Responde a solturas de tecla.'''
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+
 def check_events(ship):
 	'''Responde a eventos de pressionamento de teclas e de mouse.'''
 	for event in pygame.event.get():
@@ -8,17 +23,10 @@ def check_events(ship):
 			sys.exit()
 
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RIGHT:
-				#Move a espaçonave para a direita
-				ship.moving_right = True
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
+			check_keydown_events(event, ship)
 
 		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
+			check_keyup_events(event, ship)
 
 			#ship.rect.centerx += 1
 
